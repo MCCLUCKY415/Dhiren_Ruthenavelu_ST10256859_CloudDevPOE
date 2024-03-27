761607c1965e9c741fde1c8ef49b39fd3d49ccd4
dhire <dhire@DHIREN>
2024-03-27 16:06:27 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10256859_CLDV6211_POE_Part1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
